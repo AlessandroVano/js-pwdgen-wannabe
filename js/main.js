1cf18719aa46871370e9e406f88700dca5a410aa
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-10-20 16:03:48 +0200

    insert repo PASSWORD GENERATOR

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 /**
  * PASSWORD GENERATOR
  */
+
+//Ottenere il nome dell'utente
+const nome = prompt('Inserisci il tuo nome utente');
+console.log(nome);
+
+//Ottenere cognome dell'utente
+const cognome = prompt('Inserisci il tuo cognome utente');
+console.log(cognome);
+
+
+//Ottonere il suo colore preferito
+const colore = prompt('Quale è il tuo colore preferito?');
+console.log(colore);
+
+//Scrivere sulla pagina tutti i dati ricevuti
+// - selezionare elemento html (in questi caso "risultato" )
+// - assegnare i valori all'elemento 
+document.getElementById('risultato').innerHTML = 'La tua password è ' + nome + cognome + colore + '21';
